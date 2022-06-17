657154aed98b68ee7f50dea2e0dfd265bd59749d
iamhaitham <iamhaitham2011@gmail.com>
2022-06-17 16:59:06 +0300

    Add the required actions for onMenuTrigger function

--- scripts/menu-script.js
+++ scripts/menu-script.js
@@ -1,3 +1,19 @@
+const originalMainContent = document.getElementsByTagName('main')[0].innerHTML;
+const originalLogoStyle = document.querySelectorAll('header > p')[0].style.visibility;
+const originalFooterStyle = document.getElementsByTagName('footer')[0].style.display;
+const menuTemplate = '<object type="text/html" data="../templates/menu-template.html" ></object>';
+
 function onMenuTrigger(icon) {
     icon.classList.toggle('menuIconChanged');
+
+    const hasMenuIconChanged = icon.classList.contains('menuIconChanged');
+    
+    document.getElementsByTagName('main')[0].innerHTML = 
+        hasMenuIconChanged ? menuTemplate : originalMainContent;
+
+    document.querySelectorAll('header > p')[0].style.visibility = 
+        hasMenuIconChanged ? 'hidden' : originalLogoStyle;
+
+    document.getElementsByTagName('footer')[0].style.display = 
+        hasMenuIconChanged ? 'none' : originalFooterStyle;
 }
